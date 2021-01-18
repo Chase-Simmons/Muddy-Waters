f29d84ebbe80b1a0152c180c2588cfa0f5405b37
Chase Simmons <csim6890@gmail.com>
2021-01-17 21:18:16 -0600

    login now checks save data to verify username/pass

--- src/functions/F/login.js
+++ src/functions/F/login.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
 const write = require('write');
 
+const activateCP = (e) => {
+  e.F.readline.question('', (res) => {
+    e.F.commandParser({ res: res, F: e.F });
+  });
+};
+
 module.exports = (e) => {
-  // e.F.readline.question('Enter Username : ', (resName) => {
-  //   if (resName === 'admin') {
-  //     e.F.readline.question('Enter Password : ', (resPass) => {
-  //       if (resPass === 'password') {
-  //         setTimeout(() => {
-  //           console.log('Logging In...');
-  //         }, 500);
-  //         setTimeout(() => {
-  //           console.log('Successfully logged in.');
-  //         }, 1000);
-  //       }
-  //     });
-  //   }
-  // });
+  const saveData = e.F.save({ type: 'get' });
+  e.F.readline.question('Enter Username : ', (resName) => {
+    if (resName === saveData.login.name) {
+      e.F.readline.question('Enter Password : ', (resPass) => {
+        if (resPass === saveData.login.password) {
+          setTimeout(() => {
+            console.log('Logging In...');
+          }, 500);
+          setTimeout(() => {
+            console.log('Successfully logged in.');
+          }, 1000);
+          setTimeout(() => {
+            console.log(`hello ${saveData.login.name}!`);
+          }, 1500);
+        } else {
+          console.log('invalid password please attempt to `/login` again...');
+          activateCP(e);
+        }
+      });
+    } else {
+      console.log('invalid username please attempt to `/login` again...');
+      activateCP(e);
+    }
+  });
 };
--- src/functions/F/saveDataParser.js
+++ src/functions/F/saveDataParser.js
@@ -1,66 +1,70 @@
 module.exports = (e) => {
-  let readingGroupKey = false;
-  let readingDataKey = false;
-  let readingData = false;
-  let groupKey = '';
-  let groupKeyObj = {};
-  let dataKey = '';
-  let data = '';
-  let dataArray = [];
-  let saveData = {};
+  if (typeof e === 'string') {
+    let readingGroupKey = false;
+    let readingDataKey = false;
+    let readingData = false;
+    let groupKey = '';
+    let groupKeyObj = {};
+    let dataKey = '';
+    let data = '';
+    let dataArray = [];
+    let saveData = {};
 
-  for (let i = 0; i < e.length; i++) {
-    if (e[i] === '*') {
-      if (readingGroupKey === false) {
-        readingGroupKey = true;
-      } else {
-        readingGroupKey = false;
+    for (let i = 0; i < e.length; i++) {
+      if (e[i] === '*') {
+        if (readingGroupKey === false) {
+          readingGroupKey = true;
+        } else {
+          readingGroupKey = false;
+          readingDataKey = true;
+        }
+      } else if (e[i] === ':') {
+        readingDataKey = false;
+        readingData = true;
+      } else if (e[i] === ',') {
+        dataArray.push({ [dataKey]: data });
         readingDataKey = true;
-      }
-    } else if (e[i] === ':') {
-      readingDataKey = false;
-      readingData = true;
-    } else if (e[i] === ',') {
-      dataArray.push({ [dataKey]: data });
-      readingDataKey = true;
-      readingData = false;
-      dataKey = '';
-      data = '';
-    } else if (e[i] === '&') {
-      saveData = {
-        [groupKey]: {},
-      };
-
-      for (let index = 0; index < dataArray.length; index++) {
-        groupKeyObj = {
-          ...groupKeyObj,
-          ...dataArray[index],
+        readingData = false;
+        dataKey = '';
+        data = '';
+      } else if (e[i] === '&') {
+        saveData = {
+          [groupKey]: {},
         };
-      }
-      saveData = {
-        [groupKey]: groupKeyObj,
-      };
 
-      readingGroupKey = false;
-      readingDataKey = false;
-      readingData = false;
-    }
-    if (readingGroupKey === true) {
-      if (e[i] !== '*' && e[i] !== ':' && e[i] !== ',' && e[i] !== '&') {
-        groupKey += e[i];
+        for (let index = 0; index < dataArray.length; index++) {
+          groupKeyObj = {
+            ...groupKeyObj,
+            ...dataArray[index],
+          };
+        }
+        saveData = {
+          [groupKey]: groupKeyObj,
+        };
+
+        readingGroupKey = false;
+        readingDataKey = false;
+        readingData = false;
+      }
+      if (readingGroupKey === true) {
+        if (e[i] !== '*' && e[i] !== ':' && e[i] !== ',' && e[i] !== '&') {
+          groupKey += e[i];
+        }
+      }
+      if (readingDataKey === true) {
+        if (e[i] !== '*' && e[i] !== ':' && e[i] !== ',' && e[i] !== '&') {
+          dataKey += e[i];
+        }
+      }
+      if (readingData === true) {
+        if (e[i] !== '*' && e[i] !== ':' && e[i] !== ',' && e[i] !== '&') {
+          data += e[i];
+        }
       }
     }
-    if (readingDataKey === true) {
-      if (e[i] !== '*' && e[i] !== ':' && e[i] !== ',' && e[i] !== '&') {
-        dataKey += e[i];
-      }
-    }
-    if (readingData === true) {
-      if (e[i] !== '*' && e[i] !== ':' && e[i] !== ',' && e[i] !== '&') {
-        data += e[i];
-      }
-    }
+
+    return saveData;
+  } else if (typeof e === 'object') {
+    console.log('data needs to be unparsed');
   }
-
-  return saveData;
 };
