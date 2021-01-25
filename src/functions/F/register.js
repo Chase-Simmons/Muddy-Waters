52bb2910eb70293e939712333cca387cfa9aa537
Chase Simmons <csim6890@gmail.com>
2021-01-25 00:33:10 -0600

    wip reorganize registration

--- src/functions/F/register.js
+++ src/functions/F/register.js
@@ -1,6 +1,26 @@
-module.exports = (e) => {
-  let name;
-  let password;
+let name;
+let password;
+
+let selectedClass;
+let selectedClassStats;
+
+// let bonus = e.F.rollBonusStats();
+let trueBonus;
+
+let stat = '';
+let bonusAmt = '+';
+let usedPoints = '';
+let statBonuses = {
+  str: '',
+  dex: '',
+  vit: '',
+  def: '',
+  agi: '',
+  arc: '',
+  pie: '',
+};
+
+const HandleUserPass = (e) => {
   console.log('');
   e.F.readline.question("what's your username? : ", (resName) => {
     name = resName;
@@ -8,231 +28,229 @@ module.exports = (e) => {
       password = resPass;
       setTimeout(() => {
         console.log('');
-        console.log(
-          '|---------------------------------------------------------------------------------------------|'
-        );
-        console.log(
-          '|  account registered. now preparing character creation...                                    |'
-        );
-        console.log(
-          '|---------------------------------------------------------------------------------------------|'
-        );
+        console.log('account registered. now preparing character creation...');
+        console.log('');
       }, 500);
 
       e.F.saveDataParser({ login: { name, password } });
       e.F.save({ data: { login: { name, password } }, type: 'save' });
+
       setTimeout(() => {
-        console.log(
-          '|                please select a class by typing out its name ex. `/warrior`                  |'
-        );
-        console.log(
-          '|---------------------------------------------------------------------------------------------|'
-        );
-        console.log(
-          '|--> warrior   :   | str: 7 | dex : 1 | vit: 7 | def: 6 | agi: 2 | arc: 2 | pie: 3 | bonus: 0 |'
-        );
-        console.log(
-          '|--> cleric    :   | str: 3 | dex : 1 | vit: 6 | def: 2 | agi: 2 | arc: 5 | pie: 9 | bonus: 0 |'
-        );
-        console.log(
-          '|--> rogue     :   | str: 6 | dex : 3 | vit: 6 | def: 2 | agi: 7 | arc: 3 | pie: 1 | bonus: 0 |'
-        );
-        console.log(
-          '|--> mage      :   | str: 2 | dex : 2 | vit: 4 | def: 4 | agi: 3 | arc: 9 | pie: 4 | bonus: 0 |'
-        );
-        console.log(
-          '|--> archer    :   | str: 2 | dex : 7 | vit: 4 | def: 3 | agi: 6 | arc: 2 | pie: 4 | bonus: 0 |'
-        );
-        console.log(
-          '|--> banished  :   | str: 4 | dex : 3 | vit: 4 | def: 4 | agi: 3 | arc: 4 | pie: 1 | bonus: 5 |'
-        );
-        console.log(
-          '|---------------------------------------------------------------------------------------------|'
-        );
-        console.log('');
-        let selectedClass;
-        let selectedClassStats;
-        const selectClass = () => {
-          e.F.readline.question('', (res) => {
-            switch (res) {
-              case '/warrior':
-                selectedClass = 'warrior';
-                selectedClassStats = {
-                  str: 7,
-                  dex: 1,
-                  vit: 7,
-                  def: 6,
-                  agi: 2,
-                  arc: 2,
-                  pie: 3,
-                  bns: 0,
-                };
-                break;
-              case '/cleric':
-                selectedClass = 'cleric';
-                selectedClassStats = {
-                  str: 3,
-                  dex: 1,
-                  vit: 6,
-                  def: 2,
-                  agi: 2,
-                  arc: 5,
-                  pie: 9,
-                  bns: 0,
-                };
-                break;
-              case '/rogue':
-                selectedClass = 'rogue';
-                selectedClassStats = {
-                  str: 6,
-                  dex: 3,
-                  vit: 6,
-                  def: 2,
-                  agi: 7,
-                  arc: 3,
-                  pie: 1,
-                  bns: 0,
-                };
-                break;
-              case '/mage':
-                selectedClass = 'mage';
-                selectedClassStats = {
-                  str: 2,
-                  dex: 2,
-                  vit: 4,
-                  def: 4,
-                  agi: 3,
-                  arc: 9,
-                  pie: 4,
-                  bns: 0,
-                };
-                break;
-              case '/archer':
-                selectedClass = 'archer';
-                selectedClassStats = {
-                  str: 2,
-                  dex: 7,
-                  vit: 4,
-                  def: 3,
-                  agi: 6,
-                  arc: 2,
-                  pie: 4,
-                  bns: 0,
-                };
-                break;
-              case '/banished':
-                selectedClass = 'banished';
-                selectedClassStats = {
-                  str: 4,
-                  dex: 3,
-                  vit: 4,
-                  def: 4,
-                  agi: 3,
-                  arc: 4,
-                  pie: 1,
-                  bns: 5,
-                };
-                break;
-              default:
-                selectClass();
-                console.log(
-                  'could not match to any class names please try again.'
-                );
-                break;
-            }
-            console.log('');
-            console.log(`you have selected the class of ${selectedClass}`);
-            console.log('');
-            setTimeout(() => {
-              console.log(
-                '|---------------------------------------------------------------------------------------------|'
-              );
-              console.log(
-                '|      you will now roll your bonus stats, the banished class gets an addition 5 points       |'
-              );
-              console.log(
-                '|---------------------------------------------------------------------------------------------|'
-              );
-              console.log('');
-              e.F.readline.question(
-                "please hit the enter key whenever you're ready to roll",
-                (res) => {
-                  let bonus = e.F.rollBonusStats();
-                  const trueBonus = bonus + selectedClassStats.bns;
-                  selectedClassStats.bns = trueBonus;
-                  setTimeout(() => {
-                    console.log('');
-                    console.log(`you rolled a : ${trueBonus}`);
-                    console.log('');
-                    console.log(
-                      '|---------------------------------------------------------------------------------------------|'
-                    );
-                    console.log(
-                      '|             you can now distribute your bonus stats. ex. `/str 1` or `/pie 3`.              |'
-                    );
-                    console.log(
-                      '|---------------------------------------------------------------------------------------------|'
-                    );
-                    setTimeout(() => {
-                      console.log('');
-                      console.log(
-                        `current stats  |  bonus points left : ${selectedClassStats.bns}`
-                      );
-                      console.log(`--<> str: ${selectedClassStats.str}`);
-                      console.log(`--<> dex: ${selectedClassStats.dex}`);
-                      console.log(`--<> vit: ${selectedClassStats.vit}`);
-                      console.log(`--<> def: ${selectedClassStats.def}`);
-                      console.log(`--<> agi: ${selectedClassStats.agi}`);
-                      console.log(`--<> arc: ${selectedClassStats.arc}`);
-                      console.log(`--<> pie: ${selectedClassStats.pie}`);
-                      console.log('');
-                    }, 1000);
-                    const handleBonusDistribution = () => {
-                      e.F.readline.question('', (res) => {
-                        let stat = '';
-                        let bonusAmt = '+';
-                        let usedPoints = '';
-                        let statBonuses = {
-                          str: '',
-                          dex: '',
-                          vit: '',
-                          def: '',
-                          agi: '',
-                          arc: '',
-                          pie: '',
-                        };
-                        for (let i = 0; i < res.length; i++) {
-                          if (i > 0 && i < 4) {
-                            stat += res[i];
-                          } else if (i === 4) {
-                            bonusAmt += res[i];
-                          } else if (i > 4) {
-                            bonusAmt += res[i];
-                            usedPoints += res[i];
-                          }
-                        }
-
-                        console.log(bonus, usedPoints);
-                        bonus = parseInt(bonus) - parseInt(usedPoints);
-
-                        statBonuses = {
-                          ...statBonuses,
-                          [stat]: bonusAmt,
-                        };
-
-                        console.log(bonus, usedPoints);
-                      });
-                    };
-
-                    handleBonusDistribution();
-                  }, 1000);
-                }
-              );
-            }, 500);
-          });
-        };
-        selectClass();
+        classSelection(e);
       }, 1000);
     });
   });
+};
+
+const classSelection = (e) => {
+  console.log(
+    '|---------------------------------------------------------------------------------------------|'
+  );
+  console.log(
+    '|                please select a class by typing out its name ex. `/warrior`                  |'
+  );
+  console.log(
+    '|---------------------------------------------------------------------------------------------|'
+  );
+  console.log(
+    '|--> warrior   :   | str: 7 | dex : 1 | vit: 7 | def: 6 | agi: 2 | arc: 2 | pie: 3 | bonus: 0 |'
+  );
+  console.log(
+    '|--> cleric    :   | str: 3 | dex : 1 | vit: 6 | def: 2 | agi: 2 | arc: 5 | pie: 9 | bonus: 0 |'
+  );
+  console.log(
+    '|--> rogue     :   | str: 6 | dex : 3 | vit: 6 | def: 2 | agi: 7 | arc: 3 | pie: 1 | bonus: 0 |'
+  );
+  console.log(
+    '|--> mage      :   | str: 2 | dex : 2 | vit: 4 | def: 4 | agi: 3 | arc: 9 | pie: 4 | bonus: 0 |'
+  );
+  console.log(
+    '|--> archer    :   | str: 2 | dex : 7 | vit: 4 | def: 3 | agi: 6 | arc: 2 | pie: 4 | bonus: 0 |'
+  );
+  console.log(
+    '|--> banished  :   | str: 4 | dex : 3 | vit: 4 | def: 4 | agi: 3 | arc: 4 | pie: 1 | bonus: 5 |'
+  );
+  console.log(
+    '|---------------------------------------------------------------------------------------------|'
+  );
+  console.log('');
+
+  e.F.readline.question('', (res) => {
+    switch (res) {
+      case '/warrior':
+        selectedClass = 'warrior';
+        selectedClassStats = {
+          str: 7,
+          dex: 1,
+          vit: 7,
+          def: 6,
+          agi: 2,
+          arc: 2,
+          pie: 3,
+          bns: 0,
+        };
+        break;
+      case '/cleric':
+        selectedClass = 'cleric';
+        selectedClassStats = {
+          str: 3,
+          dex: 1,
+          vit: 6,
+          def: 2,
+          agi: 2,
+          arc: 5,
+          pie: 9,
+          bns: 0,
+        };
+        break;
+      case '/rogue':
+        selectedClass = 'rogue';
+        selectedClassStats = {
+          str: 6,
+          dex: 3,
+          vit: 6,
+          def: 2,
+          agi: 7,
+          arc: 3,
+          pie: 1,
+          bns: 0,
+        };
+        break;
+      case '/mage':
+        selectedClass = 'mage';
+        selectedClassStats = {
+          str: 2,
+          dex: 2,
+          vit: 4,
+          def: 4,
+          agi: 3,
+          arc: 9,
+          pie: 4,
+          bns: 0,
+        };
+        break;
+      case '/archer':
+        selectedClass = 'archer';
+        selectedClassStats = {
+          str: 2,
+          dex: 7,
+          vit: 4,
+          def: 3,
+          agi: 6,
+          arc: 2,
+          pie: 4,
+          bns: 0,
+        };
+        break;
+      case '/banished':
+        selectedClass = 'banished';
+        selectedClassStats = {
+          str: 4,
+          dex: 3,
+          vit: 4,
+          def: 4,
+          agi: 3,
+          arc: 4,
+          pie: 1,
+          bns: 5,
+        };
+        break;
+      default:
+        selectClass();
+        console.log('could not match to any class names please try again.');
+        break;
+    }
+    console.log('');
+    console.log(`you have selected the class of ${selectedClass}`);
+    console.log('');
+
+    trueBonus = bonus + selectedClassStats.bns;
+    setTimeout(() => {
+      handleBonusStats();
+    }, 1000);
+  });
+};
+
+const handleBonusStats = () => {};
+module.exports = (e) => {
+  HandleUserPass(e);
+  //   setTimeout(() => {
+  //     setTimeout(() => {
+  //       console.log(
+  //         '|---------------------------------------------------------------------------------------------|'
+  //       );
+  //       console.log(
+  //         '|      you will now roll your bonus stats, the banished class gets an addition 5 points       |'
+  //       );
+  //       console.log(
+  //         '|---------------------------------------------------------------------------------------------|'
+  //       );
+  //       console.log('');
+  //       e.F.readline.question(
+  //         "please hit the enter key whenever you're ready to roll",
+  //         (res) => {
+  //           selectedClassStats.bns = trueBonus;
+  //           setTimeout(() => {
+  //             console.log('');
+  //             console.log(`you rolled a : ${trueBonus}`);
+  //             console.log('');
+  //             console.log(
+  //               '|---------------------------------------------------------------------------------------------|'
+  //             );
+  //             console.log(
+  //               '|             you can now distribute your bonus stats. ex. `/str 1` or `/pie 3`.              |'
+  //             );
+  //             console.log(
+  //               '|---------------------------------------------------------------------------------------------|'
+  //             );
+  //             setTimeout(() => {
+  //               console.log('');
+  //               console.log(
+  //                 `current stats  |  bonus points left : ${selectedClassStats.bns}`
+  //               );
+  //               console.log(`--<> str: ${selectedClassStats.str}`);
+  //               console.log(`--<> dex: ${selectedClassStats.dex}`);
+  //               console.log(`--<> vit: ${selectedClassStats.vit}`);
+  //               console.log(`--<> def: ${selectedClassStats.def}`);
+  //               console.log(`--<> agi: ${selectedClassStats.agi}`);
+  //               console.log(`--<> arc: ${selectedClassStats.arc}`);
+  //               console.log(`--<> pie: ${selectedClassStats.pie}`);
+  //               console.log('');
+  //             }, 1000);
+
+  //             const handleBonusDistribution = () => {
+  //               e.F.readline.question('', (res) => {
+  //                 for (let i = 0; i < res.length; i++) {
+  //                   if (i > 0 && i < 4) {
+  //                     stat += res[i];
+  //                   } else if (i === 4) {
+  //                     bonusAmt += res[i];
+  //                   } else if (i > 4) {
+  //                     bonusAmt += res[i];
+  //                     usedPoints += res[i];
+  //                   }
+  //                 }
+
+  //                 trueBonus = parseInt(trueBonus) - parseInt(usedPoints);
+
+  //                 statBonuses = {
+  //                   ...statBonuses,
+  //                   [stat]: bonusAmt,
+  //                 };
+  //                 if (trueBonus > 0) {
+  //                   console.log(statBonuses);
+  //                   handleBonusDistribution();
+  //                 } else {
+  //                 }
+  //               });
+  //             };
+
+  //             handleBonusDistribution();
+  //           }, 1000);
+  //         }
+  //       );
+  //     }, 500);
+  //   });
+  // };
+  /*-----> BONUS STAT ROLL/DISTRIBUTION <-----*/
 };
--- src/functions/F/rollBonusStats.js
+++ src/functions/F/rollBonusStats.js
@@ -1,24 +1,22 @@
+const ranNum = Math.floor(Math.random() * 10000 + 1);
+
 module.exports = () => {
   let bonus;
-  let hasRolled = false;
-  if (hasRolled === false) {
-    const ranNum = Math.floor(Math.random() * 10000 + 1);
-    if (ranNum <= 5000) {
-      bonus = Math.floor(Math.random() * 4) + 1;
-    } else if (ranNum <= 6000) {
-      bonus = Math.floor(Math.random() * 4) + 2;
-    } else if (ranNum <= 7000) {
-      bonus = Math.floor(Math.random() * 4) + 3;
-    } else if (ranNum <= 8000) {
-      bonus = Math.floor(Math.random() * 5) + 4;
-    } else if (ranNum <= 9000) {
-      bonus = Math.floor(Math.random() * 5) + 5;
-    } else if (ranNum <= 9999) {
-      bonus = Math.floor(Math.random() * 5) + 6;
-    } else if (ranNum === 10000) {
-      bonus = 13;
-    }
-    hasRolled = true;
+  if (ranNum <= 5000) {
+    bonus = Math.floor(Math.random() * 4) + 1;
+  } else if (ranNum <= 6000) {
+    bonus = Math.floor(Math.random() * 4) + 2;
+  } else if (ranNum <= 7000) {
+    bonus = Math.floor(Math.random() * 4) + 3;
+  } else if (ranNum <= 8000) {
+    bonus = Math.floor(Math.random() * 5) + 4;
+  } else if (ranNum <= 9000) {
+    bonus = Math.floor(Math.random() * 5) + 5;
+  } else if (ranNum <= 9999) {
+    bonus = Math.floor(Math.random() * 5) + 6;
+  } else if (ranNum === 10000) {
+    bonus = 13;
   }
+
   return bonus;
 };
