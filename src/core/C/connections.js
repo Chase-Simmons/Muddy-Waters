57fe75ad8eeedc353a7d46aaebe7b05245a5881f
Chase Simmons <csim6890@gmail.com>
2021-03-11 04:08:32 -0600

    prepping tile mapping for non city locs

--- src/core/C/connections.js
+++ src/core/C/connections.js
@@ -2,25 +2,33 @@ module.exports = [
   {
     data: {
       loc: 'c1',
+      length: '1',
       connections: ['r1', 'r2'],
+      tileToConnection: ['1', '1'],
     },
   },
   {
     data: {
       loc: 'c2',
+      length: '1',
       connections: ['r2'],
+      tileToConnection: ['1'],
     },
   },
   {
     data: {
       loc: 'r1',
+      length: '12',
       connections: ['c1'],
+      tileToConnection: ['1'],
     },
   },
   {
     data: {
       loc: 'r2',
+      length: '12',
       connections: ['c1', 'c2'],
+      tileToConnection: ['1', '12'],
     },
   },
 ];
--- src/core/C/routes.js
+++ src/core/C/routes.js
@@ -3,35 +3,30 @@ module.exports = [
     data: {
       name: 'route 1',
       loc: 'r1',
-      length: '12',
     },
   },
   {
     data: {
       name: 'route 2',
       loc: 'r2',
-      length: '22',
     },
   },
   {
     data: {
       name: 'route 3',
-      loc: 'r2',
-      length: '22',
+      loc: 'r3',
     },
   },
   {
     data: {
       name: 'route 4',
-      loc: 'r2',
-      length: '22',
+      loc: 'r4',
     },
   },
   {
     data: {
       name: 'route 5',
-      loc: 'r2',
-      length: '22',
+      loc: 'r5',
     },
   },
 ];
